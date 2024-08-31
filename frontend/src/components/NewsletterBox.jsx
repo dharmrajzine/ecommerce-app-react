7a629774c7e274e4c2c847dff680dd34a820c2e8
dharmrajzine <dharmrajzine@gmail.com>
2024-09-01 01:00:44 +0530

    navbar,collection,bestoffer

---
+++ frontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+  return (
+    <div className='text-center'>
+      <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off </p>
+      <p className="text-gray-400 mt-3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, molestiae!
+      </p>
+      <form>
+            <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required />
+            <button >SUBSCRIBE</button>
+      </form>
+    </div>
+  )
+}
+
+export default NewsletterBox
